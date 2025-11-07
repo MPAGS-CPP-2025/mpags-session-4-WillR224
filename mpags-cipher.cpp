dfa236dc102962ae00cf36cab99e1b44aaa2e2c6
WillR224 <will.rose.224@gmail.com>
2025-11-07 11:08:50 +0000

    Added the command line argument handling and checking for the --help and -h flags

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,58 +1,67 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
 
-int main()
+int main(int argc, char* argv[])
 {
-    // Initialise variables
-    char c{'x'};
-    std::string out_text;
+    const std::vector<std::string> cmdLineArgs{argv, argv + argc};
+    for (int i{0}; i < cmdLineArgs.size(); i++) {
+        if (cmdLineArgs[i] == "--help" || cmdLineArgs[i] == "-h") {
+            std::cout << "Enter anything after the executable to have it returned" << std::endl;
+            return 0;
+        }
+        std::cout << cmdLineArgs[i] << std::endl;
+        }
+        // Initialise variables
+        char c{'x'};
+        std::string out_text;
 
-    // loop over each character from user input
-    while (std::cin >> c) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(c)) {
-            out_text += std::toupper(c);
-            continue;
+        // loop over each character from user input
+        while (std::cin >> c) {
+            // Uppercase alphabetic characters
+            if (std::isalpha(c)) {
+                out_text += std::toupper(c);
+                continue;
+            }
+
+            // Transliterate digits to English words
+            switch (c) {
+                case '0':
+                    out_text += "ZERO";
+                    break;
+                case '1':
+                    out_text += "ONE";
+                    break;
+                case '2':
+                    out_text += "TWO";
+                    break;
+                case '3':
+                    out_text += "THREE";
+                    break;
+                case '4':
+                    out_text += "FOUR";
+                    break;
+                case '5':
+                    out_text += "FIVE";
+                    break;
+                case '6':
+                    out_text += "SIX";
+                    break;
+                case '7':
+                    out_text += "SEVEN";
+                    break;
+                case '8':
+                    out_text += "EIGHT";
+                    break;
+                case '9':
+                    out_text += "NINE";
+                    break;
+            }
+
+            // If the character isn't alphabetic or numeric, DONT add it
         }
 
-        // Transliterate digits to English words
-        switch (c) {
-            case '0':
-                out_text += "ZERO";
-                break;
-            case '1':
-                out_text += "ONE";
-                break;
-            case '2':
-                out_text += "TWO";
-                break;
-            case '3':
-                out_text += "THREE";
-                break;
-            case '4':
-                out_text += "FOUR";
-                break;
-            case '5':
-                out_text += "FIVE";
-                break;
-            case '6':
-                out_text += "SIX";
-                break;
-            case '7':
-                out_text += "SEVEN";
-                break;
-            case '8':
-                out_text += "EIGHT";
-                break;
-            case '9':
-                out_text += "NINE";
-                break;
-        }
-
-        // If the character isn't alphabetic or numeric, DONT add it
+        // Print out the transliterated text
+        std::cout << out_text << std::endl;
     }
-
-    // Print out the transliterated text
-    std::cout << out_text << std::endl;
-}
