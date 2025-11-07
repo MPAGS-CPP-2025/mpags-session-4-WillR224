35331a24be22228598e656d1371a3cdc7f884e7f
WillR224 <will.rose.224@gmail.com>
2025-11-07 12:19:02 +0000

    Added the input and output flags to stop repeating the file input and output.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,6 +6,8 @@
 int main(int argc, char* argv[])
 {
     const std::vector<std::string> cmdLineArgs{argv, argv + argc};
+    bool inputflag{false};
+    bool outputflag{false};
     for (int i{1}; i < cmdLineArgs.size(); i++) {
         if (cmdLineArgs[i] == "--help" || cmdLineArgs[i] == "-h") {
             std::cout << "Usage: mpags-cipher [-h/--help]" << std::endl;
@@ -14,7 +16,7 @@ int main(int argc, char* argv[])
         if (cmdLineArgs[i] == "--version" || cmdLineArgs[i] == "-v") {
             std::cout << "mpags-cipher version 1.0" << std::endl;
         }
-        if (cmdLineArgs[i] == "-i") {
+        if (cmdLineArgs[i] == "-i" && inputflag == false) {
             if (i + 1 == cmdLineArgs.size() || cmdLineArgs[i + 1][0] == '-'){
                 std::cerr << "Error: No input file specified" << std::endl;
                 return 1;
@@ -22,8 +24,14 @@ int main(int argc, char* argv[])
             std::string input_file = cmdLineArgs[i + 1];
             std::cout << "Input file set to: " << input_file << std::endl;
             std::cout << "Input file handling not yet supported" << std::endl;
+            inputflag = true;
         }
-        if (cmdLineArgs[i] == "-o") {
+        else if (cmdLineArgs[i] == "-i" && inputflag == true){
+            std::cerr << "Error: Input file already specified" << std::endl;
+            return 1;
+        }
+        
+        if (cmdLineArgs[i] == "-o" && outputflag == false) {
             if (i + 1 == cmdLineArgs.size() || cmdLineArgs[i + 1][0] == '-'){
                 std::cerr << "Error: No output file specified" << std::endl;
                 return 1;
@@ -31,6 +39,11 @@ int main(int argc, char* argv[])
             std::string output_file = cmdLineArgs[i + 1];
             std::cout << "Output file set to: " << output_file << std::endl;
             std::cout << "Output file handling not yet supported" << std::endl;
+            outputflag = true;
+        }
+        else if (cmdLineArgs[i] == "-o" && outputflag == true){
+            std::cerr << "Error: Output file already specified" << std::endl;
+            return 1;
         }
     }
     return 0;
